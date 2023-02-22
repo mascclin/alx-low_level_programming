d9f4f8c0f01072f12ebaadca2dbf971932f58f5a
root <masungamascline518@gmail.com>
2023-02-22 02:36:30 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,25 +2,16 @@
 
 /**
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
- * followed by a new line
  */
-void print_alphabet_x10(void);
-(
-	Char (ch)
-	int 1;
 
-	i = 0;
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char letter;
 
-	while (i < 10)
+	while (count++ <= 9)
 	{
-		ch = 'a';
-		while (ch <= 'z')
-		{
-			_putchar(ch);
-			ch++;
-
-		}
+		for (letter = 'a' ; letter <= 'z' ; letter++)
+			_putchar(letter);
 		_putchar('\n');
-		i++;
 	}
-}
