1d1a2815a71f9b035c0b0ac00d8d8bda3b1f1da7
root <masungamascline518@gmail.com>
2023-02-24 05:37:35 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,17 @@
-#include
+#include "main.h"
 
 /**
-* _isupper.c - check numbers are 0 - 9
-* @c: Char to be checked
-*
-* Return: 0 or 1
-*/
+ * _isupper - uppercase letters
+ * @c: char to check
+ *
+ * Return: 0 or 1
+ */
 
 int _isupper(int c)
 {
-if (c >= '0' && c <= '9')
+if (c >= 'A' && c <= 'Z')
 return (1);
 else
 return (0);
-{
+}
+
