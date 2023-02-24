dab862bac3a3677780af8024d9ca8caa138d0c77
root <masungamascline518@gmail.com>
2023-02-23 18:49:46 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include
+
+/**
+* _isupper.c - check numbers are 0 - 9
+* @c: Char to be checked
+*
+* Return: 0 or 1
+*/
+
+int _isupper(int c)
+{
+if (c >= '0' && c <= '9')
+return (1);
+else
+return (0)
+{
